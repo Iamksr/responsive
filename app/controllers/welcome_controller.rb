b0ab6a41ac7ccfb839b9029ab050d66899130515
Arvind Kushwah <arvindkushwah9@gmail.com>
2020-02-04 14:29:00 +0530

    fixed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
   end
+  def upsc
+  end
+  def trick
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
  get 'about' => "table1s#about"
  get 'video' => "royalpasses#video"
   get 'gallery' => "table1s#gallery"
+ get 'upsc' => "welcome#upsc"
+  get 'trick' => "welcome#trick"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
