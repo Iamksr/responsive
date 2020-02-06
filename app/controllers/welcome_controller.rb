449aed4f4d96df8954b80c57a697fed5db6f0361
Arvind Kushwah <arvindkushwah9@gmail.com>
2020-02-06 19:24:24 +0530

    fixed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,4 +5,6 @@ class WelcomeController < ApplicationController
   end
   def trick
   end
+  def offers
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'gallery' => "table1s#gallery"
  get 'upsc' => "welcome#upsc"
   get 'trick' => "welcome#trick"
+   get 'offers' => "welcome#offers"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
